17d2fba6de02608d5d5ec34d7da100a6a6493ba9
Kashish Vaish <kashish.vaish.cs.2018@miet.ac.in>
2021-06-01 22:53:12 +0530

    Can create a new todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,6 @@
 class Todo < ActiveRecord::Base
   def to_pleasant_string
     is_complete = completed ? "[X]" : "[ ]"
-    "#{id} #{due_date.to_s(:long)} #{todo_text} #{is_complete}"
+    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_complete}"
   end
 end
