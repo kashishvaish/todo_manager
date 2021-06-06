cd3c6f451c4c597526abb64ecd94a843ad089fc8
Kashish Vaish <kashish.vaish.cs.2018@miet.ac.in>
2021-06-06 16:16:44 +0530

    Add listing and showing user functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_pleasant_string
+  end
+end
