800d7ef9f105b6614ce048b4d9ca0bffbdda4005
Kashish Vaish <kashish.vaish.cs.2018@miet.ac.in>
2021-06-08 22:35:42 +0530

    Todos using HTML and CSS

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,30 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_complete = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_complete}"
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ? and completed = ?", Date.today, false)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    where(completed: true)
+  end
+
+  def self.add_task(h)
+    create!(todo_text: h[:todo_text], due_date: (Date.today + h[:due_in_days]), completed: false)
+  end
+
+  def self.mark_as_complete(task_id)
+    task = find(task_id)
+    task.update(completed: true)
   end
 end
