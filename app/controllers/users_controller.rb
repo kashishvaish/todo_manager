b1977f9114529d5e00a5ee44199d7b794487778a
Kashish Vaish <kashish.vaish.cs.2018@miet.ac.in>
2021-06-26 22:23:30 +0530

    User sign-in functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  def new
+    render "users/new"
+  end
 
   def index
     render plain: User.order(:id).map { |user| user.to_pleasant_string }.join("\n")
@@ -12,16 +14,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password = params[:password]
-    new_user = User.create!(
-      name: name,
-      email: email,
-      password: password,
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
     )
-    response_text = "Hey, new user is created with the id #{new_user.id}"
-    render plain: response_text
+    redirect_to "/"
   end
 
   def check
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :todos
+
   def to_pleasant_string
     "#{id}. #{name} #{email}"
   end
